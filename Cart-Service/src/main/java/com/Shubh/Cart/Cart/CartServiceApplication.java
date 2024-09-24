34b1b77598fe0c675799e9eac1333a34b2767147
ShubhMehrotra <ShubhMehro@gmail.com>
2024-09-24 12:06:09 +0530

    Cart service added

---
+++ Cart-Service/src/main/java/com/Shubh/Cart/Cart/CartServiceApplication.java
@@ -0,0 +1,13 @@
+package com.Shubh.Cart.Cart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartServiceApplication.class, args);
+	}
+
+}
